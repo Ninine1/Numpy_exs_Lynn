9dda7757d40337752e95b804a2b04d756f6d9e30
Ninine1 <lynnniat@gmail.com>
2023-09-15 12:03:26 +0200

    Add files via upload

--- Exs_numpy.py
+++ Exs_numpy.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-"""
 # EX 1
 
 A = np.array([1,5,7,8])
@@ -30,12 +29,10 @@ print(a.shape)
 print(a.size)
 print(a.dtype)
 
-"""
-
 #EX 3
 
-a = np.array([[1,2,3],[4,5,6],[7,8,9]])
-"""
+a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
 print(a[1,1])
 a[1][1] = 0
 print(a[:,-1])
@@ -48,12 +45,11 @@ print(B)
 
 A = np.where(A < 5, 0, A)
 print(A)
-"""
 
 #EX 4
 
 #1
-A = np.random.randint(0, 10, 12).reshape(4,3)
+A = np.random.randint(0, 10, 12).reshape(4, 3)
 print(A)
 
 #2
@@ -64,9 +60,70 @@ A = A.astype(np.float32)
 
 #4
 A[-1, -1] = 3
-print(A) #mais ca na pas 1 dimension ...
+#A = A.ravel() # A VERIFIER !!
+print(A)
 
 #5
+print(A.T)
+
+#6
+B = np.arange(1, 13)
+B = B.reshape(3, 4)
+print(B)
+
+#7
+C = np.concatenate((A, B))
+print(C)
+
+#8
+D = np.concatenate((A, B), axis=1)
+print(D)
+
+#9
+D = D.flatten()
+print(D)
+
+#10
+np.delete(D, -1)
+
+#Exercice Statistique avec numpy
+
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+#1
+A = np.sum(a)
+print(A)
+
+#2
+B = np.sum(a, axis=1)
+print(B)
+
+#3
+C = np.sum(a, axis= 0)
+print(C)
+
+#4
+D = np.prod(a, axis = 1)
+print(D)
+
+#5
+print(a.min())
+
+#6
+print(a.max())
+
+#7
+print(a.mean())
+
+#8
+print(a.var())
+
+#9
+print(a.std())
+
+
+
+
 
 
 
